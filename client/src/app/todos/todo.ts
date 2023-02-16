c908dea5b5fd703d2ab7c04beae4dbd900a7a213
ChenfeiP <peng0368@morris.umn.edu>
2023-02-16 15:08:21 -0600

    Create todo.ts
    Co-authored-by: Harry Lyu <lv000013@morris.umn.edu>

---
+++ client/src/app/todos/todo.ts
@@ -0,0 +1,11 @@
+export interface Todo {
+  _id: string;
+  owner: string;
+  status: boolean;
+  body: string;
+  category: TodoCategory;
+}
+
+export type TodoCategory = 'software design' | 'video games' | 'homework' | 'groceries';
+
+
