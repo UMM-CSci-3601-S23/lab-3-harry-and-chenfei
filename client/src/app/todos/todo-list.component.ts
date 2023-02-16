b2b599f8a278c1cbd559ab53666b589d830a874c
ChenfeiP <peng0368@morris.umn.edu>
2023-02-16 15:04:27 -0600

    Generate todo-list files
    Co-authored-by: Harry Lyu <lv000013@morris.umn.edu>

---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Todo, TodoCategory } from './todo';
+import { TodoService } from './todo.service';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  // These are public so that tests can reference them (.spec.ts)
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoOwner: string;
+  public todoStatus: boolean;
+  public todoBody: string;
+  public todoCategory: TodoCategory;
+  public viewType: 'card' | 'list' = 'card';
+
+
+  /**
+ serverFilteredTodos: Todo[];  /**
+   * This constructor injects both an instance of `UserService`
+   * and an instance of `MatSnackBar` into this component.
+   *
+   * @param userService the `UserService` used to get users from the server
+   * @param snackBar the `MatSnackBar` used to display feedback
+   */
+  constructor(private todoService: TodoService, private snackBar: MatSnackBar) {
+    // Nothing here – everything is in the injection parameters.
+  }
+
+  /**
+   * Get the users from the server, filtered by the role and age specified
+   * in the GUI.
+   */
+  getTodosFromServer() {
+    this.todoService.getTodos({
+      category: this.todoCategory,
+      status: this.todoStatus
+    }).subscribe(returnedTodos => {
+      // This inner function passed to `subscribe` will be called
+      // when the `Observable` returned by `getUsers()` has one
+      // or more values to return. `returnedUsers` will be the
+      // name for the array of `Users` we got back from the
+      // server.
+      this.serverFilteredTodos = returnedTodos;
+      this.updateFilter();
+    }, err => {
+      // If there was an error getting the users, log
+      // the problem and display a message.
+      console.error('We couldn\'t get the list of users; the server might be down');
+      this.snackBar.open(
+        'Problem contacting the server – try again',
+        'OK',
+        // The message will disappear after 3 seconds.
+        { duration: 3000 });
+    });
+  }
+
+  /**
+   * Called when the filtering information is changed in the GUI so we can
+   * get an updated list of `filteredUsers`.
+   */
+  public updateFilter() {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, { owner: this.todoOwner, body: this.todoBody }
+    );
+  }
+
+  /**
+   * Starts an asynchronous operation to update the users list
+   */
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+}
