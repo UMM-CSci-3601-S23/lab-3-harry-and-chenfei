1b84ef76127bc04288e22eba0d275e78702e6153
ChenfeiP <peng0368@morris.umn.edu>
2023-02-17 22:25:40 -0600

    Fix the path for todos
    Co-authored-by: Harry Lyu <lv000013@morris.umn.edu>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TodoListComponent } from './todos/todo-list.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 
@@ -8,7 +9,7 @@ import { UserProfileComponent } from './users/user-profile.component';
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'users', component: UserListComponent, title: 'Users'},
-  //{path:'todos', component: TodoListComponent, title:'Todos'},
+  {path:'todos', component: TodoListComponent, title:'Todos'},
   {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'}
 ];
 
